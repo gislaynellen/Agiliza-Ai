4afc65ef066d03578370552c78c8e9392238a3f8
Diogo-Almeida-Costa <j.diogoalmeida988@gmail.com>
2025-12-06 15:16:51 -0300

    new pages created and updated data management

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,1 +1,19 @@
+const Canvas = {
+    init: function() {
+        console.log('Canvas inicializado');
+    },
 
+    saveProject: function() {
+        const title = document.querySelector('.project-title').value;
+        alert(`Projeto "${title}" salvo localmente! (Simulação)`);
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    Canvas.init();
+
+    const saveBtn = document.querySelector('.btn-secondary i.fa-save');
+    if(saveBtn) {
+        saveBtn.parentElement.addEventListener('click', Canvas.saveProject);
+    }
+});
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,1 +1,3 @@
-
+const Utils = {
+    formatDate: (date) => new Date(date).toLocaleDateString('pt-BR')
+};
